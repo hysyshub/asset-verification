41f9fb7570d29bce9419f24b5e0e7ad1d9d6d25d
hysyshub <43474698+hysyshub@users.noreply.github.com>
2018-10-04 13:14:31 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	echo $conn_string = "host=ishv-postgresqldbserver.postgres.database.azure.com port=5432 dbname=avd user=postgresqldbuser@ishv-postgresqldbserver password=Rohit@12345 options='--client_encoding=UTF8'";
+	 $conn_string = "host=ishv-postgresqldbserver.postgres.database.azure.com port=5432 dbname=avd user=postgresqldbuser@ishv-postgresqldbserver password=Rohit@12345 options='--client_encoding=UTF8'";
 
 	$host = "cp-ht-2.webhostbox.net";
 	$port = "465";
