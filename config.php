22be45474ae6f44832153243f3e9f4b788040a04
hysyshub <43474698+hysyshub@users.noreply.github.com>
2018-10-04 13:03:39 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	$conn_string = "host=ishv-postgresqldbserver.postgres.database.azure.com port=5432 dbname=avd user=postgresqldbuser@ishv-postgresqldbserver password=Rohit@12345 options='--client_encoding=UTF8'";
+
+	$host = "cp-ht-2.webhostbox.net";
+	$port = "465";
+
+	$emailUsername = "avsadmin@myangels.co.in";
+	$emailPassword = "avsadmin@2018";
+	$fromname = "contact@avapp.xyz";
+
+?>
